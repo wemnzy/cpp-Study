926b99c2780f3975a37d0e020466e133772c32a1
wemnzy <81485884+wemnzy@users.noreply.github.com>
2021-09-14 09:39:59 +0900

    Add files via upload

---
+++ 2-1.cpp
@@ -0,0 +1,15 @@
+/*
+	소스 : SimpleC++.cpp
+	cout과 << 연산자를 이용하여 화면에 출력한다.
+*/
+
+#include <iostream>  // 한 줄 주석처리
+
+
+//C++ 프로그램은 main() 함수에서부터 실행을 시작한다.
+int main() {
+	std::cout << "Hello \n";				//화면에 Hello를 출력하고 다음 줄로 넘어감
+	std::cout << "bye" <<"endl"; //화면에 bye를 출력하고 다음 줄로 넘어감
+	return 0;							
+}
+
